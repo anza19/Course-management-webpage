9b8b2d76572de7c4db05693998691c803f0407cb
Muhammad Anza Khan <muhammadanza.khan@ceridian.com>
2021-10-03 15:10:00 -0400

    completed module 8: react routing

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,21 @@ import HomePage from "./Homepage";
 import AboutPage from "./AboutPage";
 import CoursesPage from "./CoursesPage";
 import Header from "./common/Header";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
+import NotFoundPage from "./NotFoundPage";
+import ManageCoursePage from "./ManageCoursePage";
 
 const App = () => {
   return (
     <div className="container-fluid">
       <Header />
-      <Route path="/" exact component={HomePage} />
-      <Route path="/courses" exact component={CoursesPage} />
-      <Route path="/about" exact component={AboutPage} />
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/courses" component={CoursesPage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/course/:slug" component={ManageCoursePage} />
+        <Route component={NotFoundPage} />
+      </Switch>
     </div>
   );
 };
