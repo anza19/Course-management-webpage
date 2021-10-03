de91c213bad1831cae57df348d7f8336b167f4ca
Muhammad Anza Khan <muhammadanza.khan@ceridian.com>
2021-10-03 14:10:55 -0400

    adding routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,19 @@
-import React from 'react';
-import HomePage from './Homepage';
-import AboutPage from './AboutPage';
-import CoursesPage from './CoursesPage';
-import Header from './common/Header';
+import React from "react";
+import HomePage from "./Homepage";
+import AboutPage from "./AboutPage";
+import CoursesPage from "./CoursesPage";
+import Header from "./common/Header";
+import { Route } from "react-router-dom";
 
 const App = () => {
-
-    const getPage = () => {
-        const route = window.location.pathname;
-        if(route === "/about") return <AboutPage />;
-        if(route === "/courses") return <CoursesPage />;
-
-        return <HomePage />; 
-    };
-
-    return( 
-        <div className="container-fluid">
-            <Header />
-            {getPage()}
-        </div>
-    );
+  return (
+    <div className="container-fluid">
+      <Header />
+      <Route path="/" exact component={HomePage} />
+      <Route path="/courses" exact component={CoursesPage} />
+      <Route path="/about" exact component={AboutPage} />
+    </div>
+  );
 };
 
 export default App;
